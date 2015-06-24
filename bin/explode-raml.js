077f81c8f53d1acf719412c6de758c432e9c6336
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2015-06-24 17:15:07 +0200

    made the validation script sync.  Still buggy.

--- bin/explode-raml.js
+++ bin/explode-raml.js
@@ -4,19 +4,23 @@
 * 3. instead of http://olegilyenko.github.io/api-console/dist/?raml=/api-console/sphere/project.raml
 *    open http://olegilyenko.github.io/api-console/dist/?raml=/api-console/sphere/project-exploded.raml
 * 4. decide whether to check in the exploded files or not.
+*
+* FYI: as this is a quick build/test script and not a server application it is intentionally done synchronous
 */
+
 var fs = require('fs');
 var raml = require('raml-parser');
 var toRAML = require('raml-object-to-raml');
 var jsonSchemaDeref = require('json-schema-deref-sync');
 var traverse = require('traverse');
-var Validator = require('jsonschema').Validator;
-var jsonValidator = new Validator();
+var JSCK = require('jsck');
+var metaValidator = new JSCK.draft4(
+    JSON.parse(
+        fs.readFileSync('json-schema-draft4.json', 'utf8')
+    )
+);
 
-var options = {
-  derefSchemata: true
-};
-
+// "main" call:
 raml.loadFile('project.raml').then( function(rootNode) {
         var outputFilename = 'project-exploded';
         
@@ -28,38 +32,74 @@ raml.loadFile('project.raml').then( function(rootNode) {
         writeRAML(rootNodeDeref, outputFilename+"-dereferencedSchemata");
         writeJSON(rootNodeDeref, outputFilename+"-dereferencedSchemata");
 
+        // TODO: fix this function. 
+        // validateSchemata(rootNodeDeref);
+        
+        // TODO: this doesn't detect random invalid changes to examples (check specific example change against schema)
+        validateExamples(rootNodeDeref);
+
   }, function(error) {
-          console.log('Error parsing: ' + error);
+          console.log('Error parsing project RAML: ' + error);
 });
 
+// helpers:
 function writeRAML(rootNode, filePath){
-    fs.writeFile(filePath+".raml", toRAML(rootNode), function(err) {
-            if(err) {
-              console.log(err);
-            } else {
-              console.log("RAML (yaml) saved to " + filePath+".raml");
-            }
-        });
+    fs.writeFileSync(filePath+".raml", toRAML(rootNode));
+    console.log("RAML (yaml) saved to " + filePath+".raml");
 }
 
 function writeJSON(rootNode, filePath){
-    fs.writeFile(filePath+".json", JSON.stringify(rootNode, null, 4), function(err) {
-            if(err) {
-              console.log(err);
-            } else {
-              console.log("RAML (json) saved to " + filePath+".json");
-            }
-        });
+    fs.writeFileSync(filePath+".json", JSON.stringify(rootNode, null, 4));
+    console.log("RAML (json) saved to " + filePath+".json");
 }
 
 function derefSchemata(rootNode){
-    if(options.derefSchemata){
-        traverse(rootNode).forEach(function (node) {
-            if (this.key == "schema" && this.parent.key == "application/json" && this.isLeaf){
-                var schemaObj = JSON.parse(node);
-                var derefSchemaObj = jsonSchemaDeref(schemaObj, {baseFolder: process.cwd()+"/schemas/"});
-                this.update(JSON.stringify(derefSchemaObj, null, 4));
-            }
-        });
-    }
+    traverse(rootNode).forEach(function (node) {
+        if (this.key == "schema" && this.parent.key == "application/json" && this.isLeaf){
+            var schemaObj = JSON.parse(node);
+            var derefSchemaObj = jsonSchemaDeref(schemaObj, {baseFolder: process.cwd()+"/schemas/"});
+            this.update(JSON.stringify(derefSchemaObj, null, 4));
+        }
+    });
 }
+
+function validateSchemata(root){
+    traverse(rootNode).forEach(function (node) {
+        if (this.key == "schema" && this.parent.key == "application/json" && this.isLeaf){
+            var schemaObj = JSON.parse(node);
+            // TODO program silently breaks and stops in the following call:
+            var schemaErrors = metaValidator.validate(derefSchemaObj);
+            
+            if(!schemaErrors.valid){
+                console.log("schema NOT OK: "+this.path);
+                for(error in schemaErrors.errors){
+                    console.log(error);
+                }
+            }else{
+                console.log("schema OK: "+this.path);
+            }
+        }
+    });
+}
+
+function validateExamples(rootNode){
+    traverse(rootNode).forEach(function (node) {
+        if (this.key == "example" && this.parent.key == "application/json" && this.parent.node.hasOwnProperty("schema")){
+            var schemaObj = JSON.parse(this.parent.node.schema);
+            var exampleObj = JSON.parse(node);
+            schemaObj = jsonSchemaDeref(schemaObj, {baseFolder: process.cwd()+"/schemas/"});
+            var validator = new JSCK.draft4(schemaObj);
+            var schemaErrors = validator.validate(exampleObj);
+            if(!schemaErrors.valid){
+                console.log("example NOT OK: "+this.path);
+                for(error in schemaErrors.errors){
+                    console.log(error);
+                }
+            }else{
+                console.log("example OK:" + this.path);
+            }
+        }
+    });
+}
+
+
