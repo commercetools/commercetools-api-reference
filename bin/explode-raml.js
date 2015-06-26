699ad4972e72622fe1bae297a6e885343d0b8450
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2015-06-26 13:57:24 +0200

    Made the console output Markdown for easier reading of the result.

--- bin/explode-raml.js
+++ bin/explode-raml.js
@@ -5,8 +5,12 @@
 *    open http://olegilyenko.github.io/api-console/dist/?raml=/api-console/sphere/project-exploded.raml
 * 4. decide whether to check in the exploded files or not.
 *
+* FYI: the console output tries to be "markdownish", so you can do node bin/explode-raml.js > results.md for large results
+*
 * FYI: as this is a build/test script and not a server application it is intentionally done synchronous
 * FYI: the traversal code "eats" critical errors under circumstances -> check "done!" output and try/catch calls to libraries
+*
+* TODO: validate Markdown parts, too.
 */
 
 var fs = require('fs');
@@ -24,6 +28,7 @@ var metaValidator = new JSCK.draft4(jsonSchemaSchema);
 // FYI: the "raml.loadFile" call does already:
 // * _validate_ the RAML file against the RAML spec
 // * _inline_ the RAML file references ("!include" statements)
+console.log("# Starting RAML consistency check and explosion");
 raml.loadFile('project.raml').then( function(raml) {
         var outputFilename = 'project-exploded';
 
@@ -54,33 +59,36 @@ raml.loadFile('project.raml').then( function(raml) {
         writeJSON(dereferencedRaml, outputFilename+"-dereferencedSchemata");
 
         // confirm that the traversal hasn't eaten some error:
-        console.log("##### done! ######");
+        console.log("# done!");
 
   }, function(error) {
-          console.log('Error parsing project RAML: ' + error);
+          console.log(' * **Error parsing project RAML: ' + error + "**");
 });
 
 // ####  helpers ###:
 
 function writeRAML(rootNode, filePath){
     fs.writeFileSync(filePath+".raml", toRAML(rootNode));
-    console.log("RAML (yaml) saved to " + filePath+".raml");
+    console.log(" * RAML (yaml) saved to " + filePath+".raml");
 }
 
 function writeJSON(rootNode, filePath){
     fs.writeFileSync(filePath+".json", JSON.stringify(rootNode, null, 4));
-    console.log("RAML (json) saved to " + filePath+".json");
+    console.log(" * RAML (json) saved to " + filePath+".json");
 }
 
 function derefSchemata(rootNode){
+    console.log("## Schemata Derefefrencing");
     traverse(rootNode).forEach(function (node) {
         if (this.key == "schema" && this.parent.key == "application/json" && this.isLeaf){
 
             try{
                 var schemaObj = JSON.parse(this.parent.node.schema);
             } catch (ex){
-                console.log("# could not parse JSON of this schema: " + printRamlResponseContext(this.parent));
+                console.log(" * **could not parse JSON of this schema: " + printRamlResponseContext(this.parent) + "**");
+                console.log("```");
                 console.log(ex);
+                console.log("```");
                 return;
             }
 
@@ -91,23 +99,28 @@ function derefSchemata(rootNode){
 }
 
 function validateSchemata(rootNode){
+    console.log("## Schemata Validity Check");
     traverse(rootNode).forEach(function (node) {
         if (this.key == "schema" && this.parent.key == "application/json" && this.isLeaf){
 
             try{
                 var schemaObj = JSON.parse(this.parent.node.schema);
             } catch (ex){
-                console.log("# could not parse JSON of this schema: " + printRamlResponseContext(this.parent));
+                console.log(" * **could not parse JSON of this schema: " + printRamlResponseContext(this.parent) + "**");
+                console.log("```");
                 console.log(ex);
+                console.log("```");
                 return;
             }
 
             var schemaErrors = metaValidator.validate(schemaObj);
             if(!schemaErrors.valid){
-                console.log("# schema NOT OK: "+ printRamlResponseContext(this.parent));
+                console.log(" * **schema NOT OK: "+ printRamlResponseContext(this.parent) + "**");
+                console.log("```json");
                 console.log(JSON.stringify(schemaErrors.errors, null, 2));
+                console.log("```");
             }else{
-                console.log("# schema OK: "+ printRamlResponseContext(this.parent));
+                console.log(" * schema OK: "+ printRamlResponseContext(this.parent));
             }
             // "lint":
             this.update(JSON.stringify(schemaObj, null, 4));
@@ -116,22 +129,27 @@ function validateSchemata(rootNode){
 }
 
 function validateExamples(rootNode){
+    console.log("## Example Validity Check");
     traverse(rootNode).forEach(function (node) {
         if (this.key == "example" && this.parent.key == "application/json" && this.parent.node.hasOwnProperty("schema")){
 
             try{
                 var schemaObj = JSON.parse(this.parent.node.schema);
             } catch (ex){
-                console.log("# could not parse JSON of this schema: " + printRamlResponseContext(this.parent));
+                console.log(" * **could not parse JSON of this schema: " + printRamlResponseContext(this.parent) * "**");
+                console.log("```");
                 console.log(ex);
+                console.log("```");
                 return;
             }
 
             try{
                 var exampleObj = JSON.parse(node);
             } catch (ex){
-                console.log("# could not parse JSON of this example: " + printRamlResponseContext(this.parent));
+                console.log(" * **could not parse JSON of this example: " + printRamlResponseContext(this.parent) + "**");
+                console.log("```");
                 console.log(ex);
+                console.log("```");
                 return;
             }
 
@@ -140,17 +158,21 @@ function validateExamples(rootNode){
             try {
                 var validator = new JSCK.draft4(schemaObj);
             } catch (ex) {
-                console.log("# could not instantiate validator for schema: " + printRamlResponseContext(this.parent));
+                console.log(" * **could not instantiate validator for schema: " + printRamlResponseContext(this.parent) + "**");
+                console.log("```");
                 console.log(ex);
+                console.log("```");
                 return;
             }
 
             var schemaErrors = validator.validate(exampleObj);
             if(!schemaErrors.valid){
-                console.log("# example NOT OK: "+ printRamlResponseContext(this.parent));
+                console.log(" * **example NOT OK: " + printRamlResponseContext(this.parent) + "**");
+                console.log("```json");
                 console.log(JSON.stringify(schemaErrors.errors, null, 2));
+                console.log("```");
             }else{
-                console.log("# example OK: "+ printRamlResponseContext(this.parent));
+                console.log(" * example OK: " + printRamlResponseContext(this.parent));
             }
 
             // "lint":
@@ -163,6 +185,7 @@ function validateExamples(rootNode){
 // takes a "this" context of the traverse library and tries to make the RAML context transparent
 // assumes that the context is a RAML response description node like "application/json".
 function printRamlResponseContext(context){
+    // TODO some RAML parts have different paths ("undefined" in the results). probably the traits.
     var elements = [];
     elements.unshift(context.key); // should be the content type
     var responseTypeContext = context.parent.parent;
